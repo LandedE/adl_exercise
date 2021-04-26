29e627fd913fabd727c4105b690d7f0bec4e9c7e
LandedE <LandedE@users.noreply.github.com>
2021-04-26 14:20:45 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import {
 function App() {
 
   let [isUserLoggedIn, setIsUserLoggedIn] = useState(!!localStorage.getItem("alle_token"));
-
+  console.log("change");
   return (
     <Router>
       <NavBar
